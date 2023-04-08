3d1ddfb0aa8fe77cd9a6f1e5f3b3eaf583c33941
Bianel <bianel9815@gmail.com>
2023-04-08 15:57:04 -0400

    improves

--- primer-pod/index.js
+++ primer-pod/index.js
@@ -1,58 +1,99 @@
 // hello word express
-const express = require("express");
-const app = express();
+import express from "express";
+import os from "node:os";
+import cluster from "node:cluster";
+import jumpSearch from "./src/jumpSearch.js";
+import linearSearch from "./src/linearSearch.js";
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World! Primer Pod!"));
+const spawnWorker = (i) =>
+  new Promise((resolve) => {
+    const worker = cluster.fork({ id: i });
+    // debe esperar un segundo para que el proceso se inicie
+    setTimeout(() => {
+      resolve(worker);
+    }, 1000);
+  });
 
-function linearSearch(arr, target) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === target) {
-      return i;
+const primaryCode = async () => {
+  const app = express();
+  const cpuList = os.cpus();
+
+  const workers = await Promise.all(
+    [...Array(cpuList.length).keys()].map((i) => spawnWorker(i))
+  );
+
+  let busyWorkers = [];
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  app.use((req, res, next) => {
+    if (req.method === "GET" && req.path === "/") {
+      res.send(`
+        <form action="/jumpSearch" method="post">
+          <input type="text" name="arr" />
+          <input type="text" name="target" />
+          <button type="submit">jumpSearch</button>
+        </form>
+        <form action="/linearSearch" method="post">
+          <input type="text" name="arr" />
+          <input type="text" name="target" />
+          <button type="submit">linearSearch</button>
+        </form>
+      `);
+      return;
     }
-  }
-  return -1;
+
+    const worker = workers.find((worker) => !busyWorkers.includes(worker.id));
+    busyWorkers.push(worker.id);
+
+    worker.send({ path: req.path, method: req.method, body: req.body });
+
+    worker.once("message", (message) => {
+      // console.log("message", message);
+      res.send(message);
+      busyWorkers = busyWorkers.filter((id) => id !== worker.id);
+    });
+  });
+
+  setInterval(() => {
+    console.log("workers ocupados: 🔴", busyWorkers);
+    console.log(
+      "workers libres: 🟢",
+      workers
+        .filter((item) => !busyWorkers.includes(item.id))
+        .map((item) => item.id)
+    );
+  }, 1000);
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port} and process ${process.pid}`);
+  });
+};
+
+const workerCode = () => {
+  process.on("message", ({ path, body, method }) => {
+    // console.log("worker", process.pid, "recibió mensaje", path, body, method);
+    if (path === "/jumpSearch") {
+      const { arr, target } = body;
+      const formattedArr = arr.split(",").map((item) => parseInt(item));
+      const result = jumpSearch(formattedArr, +target);
+      process.send(`El número ${target} se encuentra en el índice ${result}`);
+    }
+    if (path === "/linearSearch") {
+      const { arr, target } = body;
+      const formattedArr = arr.split(",").map((item) => parseInt(item));
+
+      const result = linearSearch(formattedArr, +target);
+      process.send(`El número ${target} se encuentra en el índice ${result}`);
+    }
+
+    process.send({ path, body, method });
+  });
+};
+
+if (cluster.isPrimary) {
+  primaryCode();
+} else {
+  workerCode();
 }
-
-function jumpSearch(arr, target) {
-  const n = arr.length;
-  let step = Math.floor(Math.sqrt(n));
-  let prev = 0;
-
-  while (arr[Math.min(step, n) - 1] < target) {
-    prev = step;
-    step += Math.floor(Math.sqrt(n));
-    if (prev >= n) {
-      return -1;
-    }
-  }
-
-  while (arr[prev] < target) {
-    prev++;
-    if (prev === Math.min(step, n)) {
-      return -1;
-    }
-  }
-
-  if (arr[prev] === target) {
-    return prev;
-  }
-
-  return -1;
-}
-
-app.get("/jumpSearch", (req, res) => {
-  const arr = [1, 3, 5, 7, 9];
-  const target = 5;
-  const result = jumpSearch(arr, target);
-  res.send(`El número ${target} se encuentra en el índice ${result}`);
-});
-
-app.get("/linearSearch", (req, res) => {
-  const arr = [1, 3, 5, 7, 9];
-  const target = 5;
-  const result = linearSearch(arr, target);
-  res.send(`El número ${target} se encuentra en el índice ${result}`);
-});
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- segundo-pod/index.js
+++ segundo-pod/index.js
@@ -1,53 +1,103 @@
 // hello word express
-const express = require("express");
-const app = express();
+import express from "express";
+import binarySearch from "./src/binarySearch.js";
+import bubbleSort from "./src/bubbleSort.js";
+
+import os from "node:os";
+import cluster from "node:cluster";
+
 const port = 3001;
 
-function binarySearch(arr, target) {
-  let left = 0;
-  let right = arr.length - 1;
+const spawnWorker = (i) =>
+  new Promise((resolve) => {
+    const worker = cluster.fork({ id: i });
+    // debe esperar un segundo para que el proceso se inicie
+    setTimeout(() => {
+      resolve(worker);
+    }, 1000);
+  });
 
-  while (left <= right) {
-    let mid = Math.floor((left + right) / 2);
-    if (arr[mid] === target) {
-      return mid;
-    } else if (arr[mid] < target) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
+const primaryCode = async () => {
+  const app = express();
+  const cpuList = os.cpus();
+
+  const workers = await Promise.all(
+    [...Array(cpuList.length).keys()].map((i) => spawnWorker(i))
+  );
+
+  let busyWorkers = [];
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  app.use((req, res, next) => {
+    if (req.method === "GET" && req.path === "/") {
+      res.send(`
+          <form action="/binarySearch" method="post">
+            <input type="text" name="arr" />
+            <input type="text" name="target" />
+            <button type="submit">binarySearch</button>
+          </form>
+          <form action="/bubbleSort" method="post">
+            <input type="text" name="arr" />
+            <button type="submit">bubbleSort</button>
+          </form>
+        `);
+      return;
     }
-  }
 
-  return -1;
-}
+    const worker = workers.find((worker) => !busyWorkers.includes(worker.id));
+    busyWorkers.push(worker.id);
 
-app.get("/binarySearch", (req, res) => {
-  const arr = [1, 3, 5, 7, 9];
-  const target = 5;
-  const result = binarySearch(arr, target);
-  res.send(`El número ${target} se encuentra en el índice ${result}`);
-});
+    worker.send({ path: req.path, method: req.method, body: req.body });
 
-function bubbleSort(arr) {
-  const n = arr.length;
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
+    worker.once("message", (message) => {
+      // console.log("message", message);
+      res.send(message);
+      busyWorkers = busyWorkers.filter((id) => id !== worker.id);
+    });
+  });
+
+  setInterval(() => {
+    console.log("workers ocupados: 🔴", busyWorkers);
+    console.log(
+      "workers libres: 🟢",
+      workers
+        .filter((item) => !busyWorkers.includes(item.id))
+        .map((item) => item.id)
+    );
+  }, 1000);
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port} and process ${process.pid}`);
+  });
+};
+
+const workerCode = () => {
+  process.on("message", ({ path, body, method }) => {
+    // console.log("worker", process.pid, "recibió mensaje", path, body, method);
+    if (path === "/binarySearch") {
+      const { arr, target } = body;
+      const formattedArr = arr.split(",").map((item) => parseInt(item));
+      const result = binarySearch(formattedArr, +target);
+      process.send(`El número ${target} se encuentra en el índice ${result}`);
     }
-  }
-  return arr;
+    if (path === "/bubbleSort") {
+      const { arr } = body;
+      const formattedArr = arr
+        .split(",")
+        .map((item) => parseInt(item))
+        .filter((item) => !isNaN(item));
+
+      const result = bubbleSort(formattedArr);
+      process.send(`El arreglo ordenado es [${result}]`);
+    }
+
+    process.send({ path, body, method });
+  });
+};
+
+if (cluster.isPrimary) {
+  primaryCode();
+} else {
+  workerCode();
 }
-
-app.get("/bubbleSort", (req, res) => {
-  const arr = [9, 7, 5, 3, 1];
-  const result = bubbleSort(arr);
-  res.send(`El arreglo ordenado es [${result}]`);
-});
-
-app.get("/", (req, res) => res.send("Hello World! Segundo pod!"));
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
